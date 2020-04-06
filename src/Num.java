5f4a0a71cb83204310b105c71519bdb39cfa11df
Er1c02 <majesticr11@gmail.com>
2020-04-06 11:27:24 -0700

    Initial commit

---
+++ src/Num.java
@@ -0,0 +1,21 @@
+public class Num {
+    {
+        private int value;
+
+        public Num (int value)
+        {
+            this.value = value;
+        }//end constructor
+
+        public void setValue(int Value)
+        {
+            this.value = value;
+        }
+        public String toString()
+        {
+            return "Value is" + value;
+        }//end toString
+    }//end Class Num
+}
+
+
